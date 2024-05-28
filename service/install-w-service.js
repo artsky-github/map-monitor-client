f3b7685e9267a1bd7048dc654d1dba4535ba8ec9
Arthur <artsky.task@gmail.com>
2024-05-28 16:51:18 -0400

    Implemented maxRetries and maxRestarts config option keys to the constructor of the Service object

--- service/install-w-service.js
+++ service/install-w-service.js
@@ -7,6 +7,8 @@ const svc = new Service({
     description: "MAP Status Client that reads CUPPSFS log files and returns data when a change occurs.",
     script: path.join(__dirname, `${process.env.RUN_PATH}`),
     execPath: `${process.env.EXEC_PATH}`,
+    maxRetries: Number.MAX_SAFE_INTEGER,
+    maxRestarts: 3,
 });
 
 svc.on("install", () => {
